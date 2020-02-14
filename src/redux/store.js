89cb9431f9bc1a95e128ac2e9e886544a6bfc497
Martin Yankov <lutheriscomming@gmail.com>
2020-02-14 18:04:11 +0200

    Separated saga  logic into a root comp

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
-import { fetchCollectionsStart } from './shop/shop.sagas';
+import rootSaga from './root.saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(fetchCollectionsStart); // Here we pass individual sagas
+sagaMiddleware.run(rootSaga); // Here we pass individual sagas
 
 export const persistor = persistStore(store);
 
