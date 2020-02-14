a7e7f2386c21b9cde615bbc594588d55453ec136
Martin Yankov <lutheriscomming@gmail.com>
2020-02-14 17:36:11 +0200

    Clean unused Firebase collection funct

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 
 import {
   auth,
-  createUserProfileDocument,
-  addCollectionAndDocuments
+  createUserProfileDocument
+  // addCollectionAndDocuments
 } from './firebase/firebase.utils';
 
 import { setCurrentUser } from './redux/user/user.actions';
@@ -26,7 +26,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser, collectionsArray } = this.props;
+    const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
